2085167b6e4b85d16ae81491333a1b1d91eee710
Needi Developer <mrmna1917@gmail.com>
2025-01-04 17:02:11 +0500

    Add files via upload

---
+++ Tutorial 2/main.py
@@ -0,0 +1,22 @@
+if True:
+   print("This is indented!") #proper indentation
+
+my_var = 5
+My_var = 10
+print(my_var) #Output: 5
+print(My_var) #Output: 10
+
+print("Python");print("Knight") #multiple statements in one line
+print("Needi");print("Developer") #multiple statements in one line
+
+# This is a single line comment
+
+"""
+This is a multi-line comment. 
+It spans multiple lines.
+"""
+'''
+This is a multi-line comment. 
+It spans multiple lines.
+'''
+print("Multi-line, Comments!")
