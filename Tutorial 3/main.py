c19c81c685247332a1587e17b3f256f5b71d4668
Needi Developer <mrmna1917@gmail.com>
2025-01-07 19:56:54 +0500

    Add files via upload

---
+++ Tutorial 3/main.py
@@ -0,0 +1,56 @@
+#Variables
+apple_price = 100
+banana_price = 50
+mango_price = 170
+
+print("Apple price is",apple_price)
+print("Banana price is",banana_price)
+print("Mango price is",mango_price)
+
+Needi = 90
+Viewers = 80
+
+print(Needi)
+print(Viewers)
+
+
+# Rules Of Variables
+_name = "Needi"
+price1 = 45 # Valid
+# 1price = 45 # Invalid
+elon_musk_age = 50
+elon_musk_Age = 52
+print(elon_musk_age)
+
+# Vairables are Changeable
+elon_musk_age = 50
+elon_musk_age = 52
+print(elon_musk_age)
+
+# Constants
+PI = 3.14159
+print(PI)
+
+print1 = 45
+print(print1)
+
+if1 = 65
+
+# Data Types
+number = -3400000 # Int
+print(type(number))
+
+height = 5.11 # float
+print(type(height))
+
+paragraph1 = 'Needi Bhai' #str
+paragraph2 = "Needi Bhai"
+print(type(paragraph1))
+print(type(paragraph2))
+
+is_Game_Playing = True #bool
+is_Game_Playing1 = False
+print(type(is_Game_Playing))
+print(type(is_Game_Playing1))
+
+print("Needi Bhai")
