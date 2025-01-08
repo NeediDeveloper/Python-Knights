9b3bc34d3df16277e346069c9f0fd21ae13a2ac8
Needi Developer <mrmna1917@gmail.com>
2025-01-08 16:09:23 +0500

    Add files via upload

---
+++ Tutorial 4/main.py
@@ -0,0 +1,50 @@
+#implicit type conversion
+# num1 = 5
+# num2 = 5.5
+# num3 = num1 + num2
+# print(type(num1))
+# print(type(num2))
+# print(type(num3))
+# print(num3) 
+
+#explicit type conversion
+# number1 = 5
+# number2 = "5"
+# number3 = number1 + int(number2) #int()
+# print(number3)
+
+# number1 = 5.6
+# number2 = "5.65"
+# number3 = str(number1) + number2 #str()
+# print(number3)
+
+# number1 = 5.6
+# number2 = 5.4
+# number3 = int(number1) + int(number2) #float()
+# print(number3)
+
+# number1 = 0
+# number2 = 1
+# number3 = None
+# number4 = 56
+# number5 = ""
+# print(bool(number1))
+# print(bool(number2))
+# print(bool(number3))
+# print(bool(number4))
+# print(bool(number5))
+
+
+#OutPut Function
+# print("Hello","World", sep=" ")
+
+# print("Python", end="\n")
+# print("Knights")
+# print("Knights")
+
+# Input Function 
+name = input("Enter Name: ")
+print("Hello", name)
+
+age = input("Enter Age:")
+print("Hello", name)
