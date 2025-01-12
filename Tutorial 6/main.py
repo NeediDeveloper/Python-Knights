c0949fe3a39868f4c1b1d1c69e53205c83d61afe
Needi Developer <mrmna1917@gmail.com>
2025-01-12 21:22:35 +0500

    main.py

--- Tutorial 6/main.py
+++ Tutorial 6/main.py
@@ -15,87 +15,87 @@ eight
 nine
 ten"""
 
-# print(single_quote_string, double_quote_string)
-# print(triple_quote_string)
-# print(ten_lines)
+print(single_quote_string, double_quote_string)
+print(triple_quote_string)
+print(ten_lines)
 
 # String Indexing
-# numbers = "123456789"
-# print(numbers[6])
+numbers = "123456789"
+print(numbers[6])
 
 # Negative Indexing
-# numbers = "123456789"
-# print(numbers[-5])
+numbers = "123456789"
+print(numbers[-5])
 
 # Slicing
-# numbers = "0123456789"
-# print(numbers[1:6])
+numbers = "0123456789"
+print(numbers[1:6])
 
 # With Step
-# numbers = "0123456789"
-# print(numbers[0:10:3])
+numbers = "0123456789"
+print(numbers[0:10:3])
 
 # reverse string 
-# numbers = "0123456789"
-# print(numbers[::-1])
+numbers = "0123456789"
+print(numbers[::-1])
 
 # Strings Opperations
 # concatenation
-# first_name ="Needi"
-# last_name ="Developer"
-# full_name = first_name + " " + last_name
-# print(full_name) # Output: Needi Developer
+first_name ="Needi"
+last_name ="Developer"
+full_name = first_name + " " + last_name
+print(full_name) # Output: Needi Developer
 
 # Repetition
-# greet ="jojo"
-# print(greet * 9)
+greet ="jojo"
+print(greet * 9)
 
 # Membership Testing
-# email = input("Enter Your Email: ")
-# print("@" in email)
+email = input("Enter Your Email: ")
+print("@" in email)
 
-# spam = input("Enter Your Message: ")
-# print("bad" not in spam)
+spam = input("Enter Your Message: ")
+print("bad" not in spam)
 
 
 # Strings Methods
-# Message = "This is a Message"
-# print(Message.lower())
-# print(Message.upper())
+Message = "This is a Message"
+print(Message.lower())
+print(Message.upper())
 
-# Message = "  This is a Message  "
-# print(Message.strip())
-# print(Message.lstrip())
-# print(Message.rstrip())
+Message = "  This is a Message  "
+print(Message.strip())
+print(Message.lstrip())
+print(Message.rstrip())
 
-# Message = "Python is good and Python is easy"
-# print(Message.replace("Python","HTML"))
+Message = "Python is good and Python is easy"
+print(Message.replace("Python","HTML"))
 
 
-# Message = "Python is good and Python is easy"
-# print(Message.find("good"))
-# print(Message.find("bad"))
-# print(Message.index("good"))
-# print(Message.index("bad"))
+Message = "Python is good and Python is easy"
+print(Message.find("good"))
+print(Message.find("bad"))
+print(Message.index("good"))
+print(Message.index("bad"))
 
-# Message = "Python is good and Python is easy"
-# print(Message.startswith("Python"))
-# print(Message.endswith("Python"))
+Message = "Python is good and Python is easy"
+print(Message.startswith("Python"))
+print(Message.endswith("Python"))
 
-# text ="apple,banana,orange"
-# fruits = text.split(",")
-# print(fruits)
-# print("-".join(fruits))
+text ="apple,banana,orange"
+fruits = text.split(",")
+print(fruits)
+print("-".join(fruits))
 
-# text ="banana"
-# print(text.count("n"))
+text ="banana"
+print(text.count("n"))
 
-# text ="pyThon proGramming"
-# print(text.capitalize()) 
-# print(text.title())
-# print(text.swapcase()) 
+text ="pyThon proGramming"
+print(text.capitalize()) 
+print(text.title())
+print(text.swapcase()) 
 
-# text ="Python3"
-# print(text.isalpha()) 
-# print(text.isdigit())
-# print(text.isalnum()) 
+text ="Python3"
+print(text.isalpha()) 
+print(text.isdigit())
+print(text.isalnum()) 
