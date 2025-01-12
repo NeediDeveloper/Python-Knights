26f78fb2bd27318cca5db4c304bee33853842a15
Needi Developer <mrmna1917@gmail.com>
2025-01-12 21:21:15 +0500

    Add files via upload

---
+++ Tutorial 6/main.py
@@ -0,0 +1,101 @@
+single_quote_string ='Hello'
+double_quote_string ="World"
+triple_quote_string ='''Python
+Strings bohat achi hai
+Are Cool! kuch bhi likh do yaha pe'''
+
+ten_lines = """one
+two
+three
+four
+five
+six
+seven
+eight
+nine
+ten"""
+
+# print(single_quote_string, double_quote_string)
+# print(triple_quote_string)
+# print(ten_lines)
+
+# String Indexing
+# numbers = "123456789"
+# print(numbers[6])
+
+# Negative Indexing
+# numbers = "123456789"
+# print(numbers[-5])
+
+# Slicing
+# numbers = "0123456789"
+# print(numbers[1:6])
+
+# With Step
+# numbers = "0123456789"
+# print(numbers[0:10:3])
+
+# reverse string 
+# numbers = "0123456789"
+# print(numbers[::-1])
+
+# Strings Opperations
+# concatenation
+# first_name ="Needi"
+# last_name ="Developer"
+# full_name = first_name + " " + last_name
+# print(full_name) # Output: Needi Developer
+
+# Repetition
+# greet ="jojo"
+# print(greet * 9)
+
+# Membership Testing
+# email = input("Enter Your Email: ")
+# print("@" in email)
+
+# spam = input("Enter Your Message: ")
+# print("bad" not in spam)
+
+
+# Strings Methods
+# Message = "This is a Message"
+# print(Message.lower())
+# print(Message.upper())
+
+# Message = "  This is a Message  "
+# print(Message.strip())
+# print(Message.lstrip())
+# print(Message.rstrip())
+
+# Message = "Python is good and Python is easy"
+# print(Message.replace("Python","HTML"))
+
+
+# Message = "Python is good and Python is easy"
+# print(Message.find("good"))
+# print(Message.find("bad"))
+# print(Message.index("good"))
+# print(Message.index("bad"))
+
+# Message = "Python is good and Python is easy"
+# print(Message.startswith("Python"))
+# print(Message.endswith("Python"))
+
+# text ="apple,banana,orange"
+# fruits = text.split(",")
+# print(fruits)
+# print("-".join(fruits))
+
+# text ="banana"
+# print(text.count("n"))
+
+# text ="pyThon proGramming"
+# print(text.capitalize()) 
+# print(text.title())
+# print(text.swapcase()) 
+
+# text ="Python3"
+# print(text.isalpha()) 
+# print(text.isdigit())
+# print(text.isalnum()) 
