6cedeeebfcf07e75fe46f8ee4b79a2ed03352ae8
Needi Developer <mrmna1917@gmail.com>
2025-01-12 14:56:11 +0500

    Add files via upload

---
+++ Tutorial 5/main.py
@@ -0,0 +1,7 @@
+number1 = int(input("Enter First Number: "))
+number2 = int(input("Enter Second Number: "))
+
+print("Addition of numbers is :", number1 + number2)
+print("Subtraction of numbers is :", number1 - number2)
+print("Multiplication of numbers is :", number1 * number2)
+print("Division of numbers is :", number1 / number2)
