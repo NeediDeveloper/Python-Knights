056e42cf2891559e15eccc5c164c97605939a0a7
Needi Developer <mrmna1917@gmail.com>
2025-01-16 15:08:00 +0500

    Add files via upload

---
+++ Tutorial 8/main.py
@@ -0,0 +1,5 @@
+# Write a Python program that takes a string input from the user. 
+# Perform the following tasks:
+# Convert the string to uppercase.
+# Find the length of the string.
+# Replace all spaces in the string with underscores (_).
