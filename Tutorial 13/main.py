737a550c5f8e1f62e5d5ed9794adb94543678337
Needi Developer <mrmna1917@gmail.com>
2025-01-28 17:23:10 +0500

    Add files via upload

---
+++ Tutorial 13/main.py
@@ -0,0 +1,18 @@
+# ATM Simulator
+
+# Objective: Simulate a simple ATM machine with these
+# features:
+# 1.Show a menu with the following options:
+# i.Check Balance
+# ii.Withdraw Money
+# iii.Exit
+# 2.Use if-elif-else to handle the options.
+# Use a while loop to keep the program running until the
+# user selects
+# "Exit."
+# 3.
+# 4.Implement the following logic:
+# Allow withdrawal only if the balance is sufficient.
+# Use shorthand if-else to update the balance.
+# Exit the loop when the user chooses
+# "Exit."
