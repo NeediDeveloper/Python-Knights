bcd6b402c86521be082cce13e4beb15322286110
Needi Developer <mrmna1917@gmail.com>
2025-02-09 18:05:26 +0500

    Add files via upload

---
+++ Tutorial 17/main.py
@@ -0,0 +1,14 @@
+# Prime Number Check
+
+# Write a Python function to check whether a given number
+# is prime or not. A prime number is a number greater than 1
+# that has no divisors other than 1 and itself.
+# Write a function is
+# _prime() that:
+    # 1.Accepts an integer as input.
+    # 2.Returns True if the number is prime.
+    # 3.Returns False if the number is not prime.
+
+# Example Input and Output:
+    # is_prime(5) # Output: True (5 is a prime number)
+    # is_prime(10) # Output: False (10 is not a prime number)
