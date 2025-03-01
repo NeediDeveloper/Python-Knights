53dc898c0e4b3b2abda5f6d80d7253c9380e6220
Needi Developer <mrmna1917@gmail.com>
2025-03-01 12:35:57 +0500

    Update main.py

--- Tutorial 23/main.py
+++ Tutorial 23/main.py
@@ -1,15 +1,1 @@
-# students = ["Needi","Ayesha","Maheen", "Ahmad", "Ali", "Sana", "Musa"]
-students = ("Needi","Ayesha","Maheen", "Ahmad", "Ali", "Sana", "Musa")
-for index, student in enumerate(students, start=15):
-    print(f"Roll No: {index}, Student Name: {student}")
-    
-names = ["Naveed","Sara","Fatima", "Maheen"]
-scores = [90, 85, 88, 99, 45]
-for name, score in zip(names, scores):
-    print(f"{name} scored {score}")
 
-zipped = [("Naveed", 90), ("Sara", 85), ("Fatima", 88)]
-names, scores = zip(*zipped)
-
-print(names)
-print(scores)
