96e823835658ffd1c6da9a9cda00c1cca176d9d1
Needi Developer <mrmna1917@gmail.com>
2025-03-01 13:12:38 +0500

    Update main.py

--- Tutorial 23/main.py
+++ Tutorial 23/main.py
@@ -1,1 +1,14 @@
+fruits = ["apple","banana","cherry"]
+for index, fruit in enumerate(fruits, start=15):
+    print(f"Index: {index}, Fruit: {fruit}")
+    
+    
+names = ["Naveed","Sara","Fatima", "Maheen"]
+scores = [90, 85, 88, 59, 45]
+for name, score in zip(names, scores):
+    print(f"{name} scored {score}")
 
+keys = ["name", "age", "city", "country", "grade"]
+value = ["Needi Developer", 20, "Gujranwala", "Pakistan", "A"]
+dictionary = dict(zip(keys, value))
+print(dictionary)
