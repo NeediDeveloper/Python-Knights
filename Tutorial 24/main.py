bc445bf4eb0640e1b68650a427f2638f726a7f74
Needi Developer <mrmna1917@gmail.com>
2025-03-02 20:51:49 +0500

    Add files via upload

---
+++ Tutorial 24/main.py
@@ -0,0 +1,15 @@
+# Data Structures Puzzle
+
+["Ali","Naveed","Sara","Zoya","Ali","Sara"]
+(85, 90, 78, 88, 85, 78)
+
+# Create a program that performs the following tasks:
+#     1.You have two data structures:
+#         A list of students:
+#             ["Ali","Naveed","Sara","Zoya","Ali","Sara"]
+#         A tuple of their marks: (85, 90, 78, 88, 85, 78)
+#     2.Perform these operations:
+#         Find unique students (remove duplicates).
+#         Combine students and marks into a dictionary
+#         where names are keys, and marks are values
+#         (for duplicate names, keep the latest mark).
