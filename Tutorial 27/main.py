0d398036fb90df19d11e7381ce7fca00956208c3
Needi Developer <mrmna1917@gmail.com>
2025-03-10 17:25:12 +0500

    Add files via upload

---
+++ Tutorial 27/main.py
@@ -0,0 +1,27 @@
+def calculate_sum(a, b):
+    print(f"a: {a}, b: {b}")
+    result = a + b
+    print(f"result: {result}")
+    return result
+
+sum_result = calculate_sum(5, 3)
+print(f"Sum is: {sum_result}")
+
+def counter1():
+    count = 0
+    count += 1
+    count += 1
+    count += 2
+    count += 1
+    print(f"Counter 1 is: {count}")
+counter1()
+    
+def counter2():
+    count = 0
+    count += 6
+    count += 8
+    count += 2
+    count += 1
+    print(f"Counter 2 is: {count}")
+counter2()
+    
