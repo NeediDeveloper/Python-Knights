5ec2697cb1032b069efbca083dc0f1792d5aef95
Needi Developer <mrmna1917@gmail.com>
2025-03-22 16:40:36 +0500

    Add files via upload

---
+++ tutorial 30/main.py
@@ -0,0 +1,6 @@
+# with open("file.txt","r") as file:
+#     content = file.readlines()
+#     print(content[1])
+
+# with open("file.txt","w") as file:
+#     file.write("This is adding by main.py file")
