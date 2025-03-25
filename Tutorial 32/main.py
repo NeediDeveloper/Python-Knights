1e33f2862cd24e637560863dbcf6aaf7996e944d
Needi Developer <mrmna1917@gmail.com>
2025-03-25 22:57:14 +0500

    Add files via upload

---
+++ Tutorial 32/main.py
@@ -0,0 +1,16 @@
+# File-Based To-Do List Program
+    # Ek simple program banaye jisme user apne tasks ko add,
+    # view, aur delete kar sakta hai. Ye tasks file mein save
+    # honge, taki program band hone ke baad bhi data safe
+    # rahe.
+    # Program user ko menu options de:
+        # Add a new task: Naya task add kare.
+        # View all tasks: Saare tasks dikhaye.
+        # Delete a task by number: Task number ke zariye
+        # delete kare.
+        # Exit the program: Program ko band kare.
+    # File Handling:
+        # Tasks ek file (todo.txt) mein store kare.
+        # Jab program shuru ho, tab file se tasks read kare.
+        # Jab user koi action kare (add ya delete), tab file ko
+        # update kare.
