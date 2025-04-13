ef69f405edc55aa641c65e6d3ef8e35e98d3084b
Needi Developer <mrmna1917@gmail.com>
2025-04-13 17:56:12 +0500

    Add files via upload

---
+++ Tutorial 40/main.py
@@ -0,0 +1,11 @@
+# Build a Simple Banking System
+
+# Create a banking system using object-oriented programming in Python. The system should allow a user to perform the following actions:
+    # 1.Check Balance: Display the current account balance.
+    # 2.Deposit Money: Add money to the account.
+    # 3.Withdraw Money: Deduct money from the account if the balance is sufficient.
+    # 4.Exit: Close the application.
+# Requirements:
+    # Use a class BankAccount to represent the bank account.
+    # Implement methods for each operation (check_balance, deposit, withdraw).
+    # Use a simple loop to allow the user to perform multiple actions until they choose to exit.
