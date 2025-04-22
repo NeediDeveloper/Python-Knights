8cc005c642aef5af3c250e9fb615a65924c4a3e2
Needi Developer <mrmna1917@gmail.com>
2025-04-22 18:11:48 +0500

    Add files via upload

---
+++ Tutorial 44/main.py
@@ -0,0 +1,17 @@
+# File Management Script
+
+# Create a Directory:
+#     Use the os module to create a directory named RandomFiles in the current working directory.
+# Generate Random Files:
+#     Use the random module to generate 5 random filenames.
+#     Each filename should be a random number between 1000 and 9999 with a .txt extension.
+#     Example filenames: 1054.txt, 8762.txt.
+
+# Write Data to Files:
+#     For each file, generate a random number and calculate its square root using the math module.
+#     Write the random number and its square root into the file in this format:
+#         Random Number: 25
+#         Square Root: 5.0
+        
+# Display the Created Files:
+#     List all the files created in the RandomFiles directory using the os module.
